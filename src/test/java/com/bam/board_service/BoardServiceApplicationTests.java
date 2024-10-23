1f155185acf202f6280a633959ff92e57f2c7906
bam <juhyun19981129@gmail.com>
2024-10-23 20:18:13 +0900

    첫 커밋

---
+++ src/test/java/com/bam/board_service/BoardServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bam.board_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoardServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
