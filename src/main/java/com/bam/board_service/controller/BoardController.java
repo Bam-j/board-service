a828f112e469c3fc13c203690da2756225f571b1
bam <juhyun19981129@gmail.com>
2024-11-07 18:41:04 +0900

    게시글 상세 조회 구현을 위한 컨트롤러 구현

--- src/main/java/com/bam/board_service/controller/BoardController.java
+++ src/main/java/com/bam/board_service/controller/BoardController.java
@@ -3,9 +3,12 @@ package com.bam.board_service.controller;
 import com.bam.board_service.dto.board.PostWriteDTO;
 import com.bam.board_service.repository.BoardRepository;
 import com.bam.board_service.service.BoardService;
+import java.util.UUID;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
@@ -24,5 +27,13 @@ public class BoardController {
         boardService.save(postWriteDTO);
 
         return "redirect:/index";
+    }
+
+    @GetMapping("/post/view/{id}")
+    public String postView(@PathVariable UUID id, Model model) {
+        model.addAttribute("post", boardRepository.findById(id));
+        boardService.increaseViews(id);
+
+        return "/board/postView";
     }
 }
--- src/main/java/com/bam/board_service/repository/BoardRepository.java
+++ src/main/java/com/bam/board_service/repository/BoardRepository.java
@@ -4,10 +4,17 @@ import com.bam.board_service.entity.PostEntity;
 import java.util.Optional;
 import java.util.UUID;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface BoardRepository extends JpaRepository<PostEntity, UUID> {
 
     Optional<PostEntity> findByWriter(String writer);
+
+    @Modifying
+    @Query(value = "UPDATE PostEntity postEntity SET postEntity.views=postEntity.views+1 WHERE postEntity.id=:id")
+    void increaseViews(@Param("id") UUID id);
 }
